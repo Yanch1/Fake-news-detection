458ba90055bcf9f3f7d12c0f539c865277752f59
Yanch1 <janekpiotro@gmail.com>
2020-01-10 01:48:52 +0100

    data fully processed

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
-import keras_applications,keras_preprocessing
+import keras_applications, keras_preprocessing
 import fasttext
 import pandas as pf  # reading csv files
 import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+from sklearn.preprocessing import LabelEncoder
 
 # nltk.download('stopwords')
 # nltk.download('punkt')
@@ -19,20 +20,7 @@ truthRating = {
     5: 'Pants on Fire'
 }
 
-
-def remove_punctuation(data):
-
-    def remove(text):
-        blank_text = [c for c in text if c not in string.punctuation]
-        return blank_text
-
-    data['Statement'] = data['Statement'].apply(lambda x: remove(x))
-    return data
-
-
-def remove_capital_letters(data):
-    data['Statement'] = data['Statement'].apply(lambda x: x.lower())
-    return data
+stop_words = set(stopwords.words('english'))
 
 
 def sentiment_tokenize(data):
@@ -45,40 +33,86 @@ def words_tokenize(data):
     return data
 
 
-# works on tokens
-def remove_stopwords(data):
+def preprocess_data(data, _regular_expressions=False, _remove_capitals=False, _remove_punctuation=False,
+                    _remove_stopwords=False, _stemming=False):
+    if _regular_expressions:
+        # replace numbers with 'numbr'
+        data = data.str.replace(r'\d+(\.\d+)?', 'numbr ')
+        # replace money symbols with 'moneysymb'
+        data = data.str.replace(r'£|\$', 'moneysymb ')
 
-    def remove(text):
-        clean_list = [word for word in text if word not in set(stopwords.words('english'))]
-        return clean_list
+    if _remove_punctuation:
+        # remove punctuation
+        data = data.str.replace(r'[^\w\d\s]', ' ')
+        # replace whitespace with a single space
+        data = data.str.replace(r'\s+', ' ')
+        # remove leading / trailoring space
+        data = data.str.replace(r'%\s+|\s+?$', '')
 
-    data['Statement'] = data['Statement'].apply(lambda x: remove(x))
+    if _remove_capitals:
+        # remove capital letters
+        data = data.str.lower()
+
+    if _remove_stopwords:
+        # remove common words
+        data = data.apply(lambda x: ' '.join(word for word in x.split() if word not in stop_words))
+
+    if _stemming:
+        # set words to their basic form
+        stm = nltk.PorterStemmer()
+        data = data.apply(lambda x: ' '.join(stm.stem(word) for word in x.split()))
+
     return data
 
 
-def prepare_data(data):
-
-    s1 = remove_capital_letters(data)
-    s2 = words_tokenize(s1)
-    s3 = remove_punctuation(s2)
-    s4 = remove_stopwords(s3)
-
-    return s4
-
-
+# import data
 data = pf.read_csv('train.csv', sep='	')
 
-print(data.head())
+# split data
+ratings = data['Rating']
+statements = data['Statement']
 
-data = prepare_data(data)
+# swap truth ratings to numbers
+encoder = LabelEncoder()
+Y = encoder.fit_transform(ratings)
 
-print(data.head())
+# preprocess data
+statements = preprocess_data(statements, _regular_expressions=True, _remove_capitals=True, _remove_punctuation=True,
+                             _remove_stopwords=True, _stemming=True)
+
+# create most used words dictionary
+all_words = []
+for statement in statements:
+    words = word_tokenize(statement)
+    for w in words:
+        all_words.append(w)
+
+
+all_words = nltk.FreqDist(all_words)
+# print(f'words nr after freqdist: {len(all_words)}')
+# print(f'most common: {all_words.most_common()}')
+
+word_features = list(all_words.keys())[:2000]
+
+
+def find_features(statement):
+    words = word_tokenize(statement)
+    features = {}
+    for word in word_features:
+        features[word] = (word in words)
+
+    return features
+
+
+feature = find_features(statements[0])
+for key, value in feature.items():
+    if value:
+        print(key)
+
+
 
 # print(data.head())
-# print(data['Statement'])
-
-
-
-# 1. prepare data
-#  1.1 delete common words
-#  1.2 delete punctuation
+#
+# data = preprocess_data(data)
+#
+# print(data.head())
